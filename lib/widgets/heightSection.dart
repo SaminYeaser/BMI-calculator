643c2bbe393972f0d7f45aa14480cdc980774f0d
SaminYeaser <saminyeaser1@gmail.com>
2021-06-07 06:03:13 +0600

    slider added

--- lib/widgets/heightSection.dart
+++ lib/widgets/heightSection.dart
@@ -1,23 +1,51 @@
 import 'package:bmi/colors/colorsSection.dart';
 import 'package:flutter/material.dart';
 
-class HeightSection extends StatelessWidget {
+class HeightSection extends StatefulWidget {
+  @override
+  _HeightSectionState createState() => _HeightSectionState();
+}
+
+class _HeightSectionState extends State<HeightSection> {
+  int height = 180;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Padding(padding: EdgeInsets.only(top: 20),
-        child: Center(child: Text('Height',))),
-        SizedBox(height: 10,),
+        Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Center(
+                child: Text(
+              'Height',
+            ))),
+        SizedBox(
+          height: 10,
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          textBaseline: TextBaseline.alphabetic,
           children: [
-            Text('180', style: HugeNumber,),
+            Text(
+              height.toString(),
+              style: HugeNumber,
+            ),
             Text('cm')
           ],
-        )
+        ),
+        Slider(
+            value: height.toDouble(),
+            min: 120,
+            max: 220,
+            activeColor: Colors.red,
+            inactiveColor: Colors.grey,
+            onChanged: (double newValue) {
+              setState(() {
+                height = newValue.toInt();
+              });
+            })
       ],
     );
   }
